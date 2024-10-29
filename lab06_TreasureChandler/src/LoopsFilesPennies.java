28575cfacb8fdff14a7733d976f1ed6434a85d76
Treasure <tchandler1211@gmail.com>
2024-10-29 16:49:59 -0400

    Finished lab06!

--- lab06_TreasureChandler/src/LoopsFilesPennies.java
+++ lab06_TreasureChandler/src/LoopsFilesPennies.java
@@ -15,11 +15,24 @@ public class LoopsFilesPennies {
      * 
      * @param args      entered values
      */
+    /*
+     * this line is mainly to suppress the "unused" warning for
+     * "dayWhenOptionIPayLessOptionII", even though the variable is
+     * used later on in the program
+     */
+    @SuppressWarnings("unused")
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // variables declaration
         int pennies = 0, days, dayWhenOptionIPayLessOptionII = 0;
         double payRateOptionI, wages = 0, difference;
         boolean optionIPayLessOptionII = false;
+
+        // problem 7/12:
+        /*
+         * creates a PrintWriter object to write to the wages.txt file, along with
+         * ensuring that the file can be appended
+         */
+        PrintWriter wagesWriter = new PrintWriter(new FileWriter("wages.txt", true));
 
         // problem 2:
         // instantiates a Scanner object to read from the console
@@ -67,6 +80,13 @@ public class LoopsFilesPennies {
             scan.nextLine();
         }
 
+        /*
+         * this simple print statement it outside of the for loop in order to
+         * print these strings in the console once
+         */
+        System.out.println("Pay Rate for Option II vs Option I:\n" +
+                            "Days Worked Option II         Cents         Option I $");
+
         // this for loop iterates the days (how many days the user inputted)
         for (int i = 1; i <= days; ++i) {
             /*
@@ -96,7 +116,15 @@ public class LoopsFilesPennies {
                 }
             }
 
-            System.out.printf("Day %2d: %,15d\t%,12.2f\n", i, pennies,
+            System.out.printf("Day %2d: \t%,15d\t\t%,12.2f\n", i, pennies,
+                                i * payRateOptionI);
+
+            // problem 15:
+            /*
+             * along with printing the table in the console, the same table will
+             * be printed in the wages.txt file
+             */
+            wagesWriter.printf("Day %2d: \t%,15d\t\t%,12.2f\n", i, pennies,
                                 i * payRateOptionI);
         }
 
@@ -107,35 +135,37 @@ public class LoopsFilesPennies {
         wages = pennies / 100.0;
 
         // display the dollar total in the console
-        System.out.printf("For %d days worked, the CS major earned $%,.2f, " + 
+        System.out.printf("\nFor %d days worked, the CS major earned $%,.2f, " + 
                             "and the XX major earned $%,2.2f", days, wages,
                             days * payRateOptionI);
 
-        // problem 4:
-        /*
-         * My Experience:
-         * 
-         * When it comes to analyzing the table between the pay rate options
-         * for the CS major and the XX major, it is clearly shown that the choice
-         * the CS major made was justifiable. If the CS major chose to double their
-         * pay rate each day, not much progress is shown. The CS major's pay rate
-         * appears to be less than the XX major's, until up to day 15. On day 14, the
-         * CS major's pay rate was at $8,192. However, due to the aformentioned plan,
-         * that pay rate doubled the next day to $16,384, which is greater than the XX
-         * major's pay rate, only at $15,000. From this day forward, the CS major's pay
-         * rate will always be greater than the XX major's, further supporting the fact
-         * that the choice made by the CS major was indeed justifiable.
-         */
+        wagesWriter.printf("\nFor %d days worked, the CS major earned $%,.2f, " +
+                            "and the XX major earned $%,2.2f", days, wages,
+                            days * payRateOptionI);
 
-        // declare the file name (such as "wages.txt" for writing in the file)
-        // String fileName;
+        // problems 4-5:
+        String comment = "\n\nWhen it comes to analyzing the table between the pay\n" +
+                        "rate options for the CS major and the XX major, it is\n" +
+                        "clearly shown that the choice the CS major made was\n" +
+                        "justifiable. If the CS major chose to double their\n" +
+                        "pay rate each day, not much progress is shown. The CS\n" +
+                        "major's pay rate appears to be less than the XX major's,\n" +
+                        "until up to day 15. On day 14, the CS major's pay rate\n" +
+                        "was at $8,192. However, due to the aforementioned plan,\n" +
+                        "that pay rate doubled the next day to $16,384, which is\n" +
+                        "greater than the XX major's pay rate, only at $15,000.\n" +
+                        "From this day forward, the CS major's pay rate will\n" +
+                        "always be greater than the XX major's, further\n" +
+                        "supporting the fact that the choice made by the CS\n" +
+                        "major was indeed justifiable.\n";
 
-        // get the actual file name
-        // System.out.println("Enter the file name, such as \"wages.txt\":");
-        // fileName = scan.nextLine();
+        // problem 8:
+        // writes the "comment" variable to the file, then closes it
+        wagesWriter.println(comment);
 
+        wagesWriter.close();
         scan.close();
-
+        
     } // end of main()
 
 } // end of LoopsFilesPennies
